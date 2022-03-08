50b385ebb2d9e231e49b739a7025931e4736dcb5
root <root@localhost.localdomain>
2022-03-08 13:41:07 -0500

    Fixed foundation-python for building python 3 on Rocks 8.0

--- src/foundation-python/version.mk
+++ src/foundation-python/version.mk
@@ -1,6 +1,6 @@
 NAME	= foundation-python
 CONFIGOPTS = --enable-ipv6 --enable-unicode=ucs4
-
+RELEASE = 0
 ifeq ($(strip $(VERSION.MAJOR)), 5)
 VERSION = 2.4.3
 RELEASE = 0
@@ -23,3 +23,15 @@ RPM.FILES = "/opt/rocks/bin/*\\n/opt/rocks/include/python2*\\n/opt/rocks/lib/lib
 ifeq ($(strip $(VERSION.MAJOR)), 7)
 RPM.FILES += "\\n/opt/rocks/lib/pkgconfig/*"
 endif 
+ifeq ($(strip $(VERSION.MAJOR)), 8)
+VERSION = 3.10.2
+RELEASE = 0
+ADDFLAGS = "CFLAGS=-fPIC"
+RPM.EXTRAS="%define _python_bytecompile_errors_terminate_build 0\\n%define __python_requires  %{_builddir}/%{name}-%{version}/filter_python_requires.sh"
+#CONFIGOPTS += --exec-prefix=$(PKGROOT)
+endif
+RPM.FILES = "/opt/rocks/bin/*\\n/opt/rocks/include/python3*\\n/opt/rocks/lib/lib*\\n/opt/rocks/lib/python3*\\n/opt/rocks/share/man/man1/*\\n/opt/rocks/usr/bin/*"
+ifeq ($(strip $(VERSION.MAJOR)), 8)
+RPM.FILES += "\\n/opt/rocks/lib/pkgconfig/*"
+endif
+
